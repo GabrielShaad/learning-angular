537a6ed57ee4ad685eb785197310c30f38d912d5
Gabriel Alexandre <gabriel.shaad@gmail.com>
2018-01-28 14:39:31 -0200

    Create controller of newsletter page

---
+++ module5-solution/src/public/newsletter/newsletter.controller.js
@@ -0,0 +1,12 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('NewsletterController', NewsletterController);
+
+// NewsletterController.$inject = ['menuItems'];
+function NewsletterController() {
+  var $ctrl = this;
+}
+
+})();
